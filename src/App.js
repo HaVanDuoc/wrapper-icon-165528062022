8cc2223323652560d6e6a8e13d1f5a7e8d82c62d
Hà Văn Được <havanduoc.dev@gmail.com>
2022-06-29 15:53:42 +0700

    Toẹt vời

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import AdbIcon from "@mui/icons-material/Adb";
 function App() {
     return (
         <div className="__havanduoc">
-            <WrapperIcon color="red" boxShadow="0 0 0 rgba(0, 0, 0, 0.2)">
+            <WrapperIcon>
                 <AdbIcon />
             </WrapperIcon>
         </div>
--- src/WrapperIcon/index.js
+++ src/WrapperIcon/index.js
@@ -1,33 +1,12 @@
-import React from "react";
+import "./WrapperIcon.scss";
 
 const WrapperIcon = (props) => {
-    const {
-        children,
-        color = "#000",
-        width = "50px",
-        height = "50px",
-        border = "1px solid #000",
-        borderRadius = "5px",
-        display = "flex",
-        justifyContent = "center",
-        alignItems = "center",
-        cursor = "pointer",
+    const { children, ...otherProps } = props;
 
-        ...otherProps
-    } = props;
     return (
         <div
-            id="WrapperIcon"
+            className="HvdWrapperIcon"
             style={{
-                color,
-                width,
-                height,
-                border,
-                borderRadius,
-                display,
-                justifyContent,
-                alignItems,
-                cursor,
                 ...otherProps,
             }}
         >
